98153ef906c08a7f11f46bec533ab53dcec96915
Maxim Selivanov <aikonradish@yandex.ru>
2023-02-01 13:08:47 +0300

    Changed the font, changed the font size, edited the interface

--- text_anal/Form1.Designer.cs
+++ text_anal/Form1.Designer.cs
@@ -39,23 +39,24 @@
             this.button1 = new System.Windows.Forms.Button();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox
             // 
+            this.textBox.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox.Location = new System.Drawing.Point(13, 20);
             this.textBox.Multiline = true;
             this.textBox.Name = "textBox";
-            this.textBox.Size = new System.Drawing.Size(677, 171);
+            this.textBox.Size = new System.Drawing.Size(813, 171);
             this.textBox.TabIndex = 0;
             this.textBox.Text = "Писатб сюда свои поэмы";
             // 
             // buttonAnalysis
             // 
+            this.buttonAnalysis.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.buttonAnalysis.Location = new System.Drawing.Point(15, 197);
             this.buttonAnalysis.Name = "buttonAnalysis";
-            this.buttonAnalysis.Size = new System.Drawing.Size(131, 37);
+            this.buttonAnalysis.Size = new System.Drawing.Size(393, 37);
             this.buttonAnalysis.TabIndex = 1;
             this.buttonAnalysis.Text = "Анализироватб";
             this.buttonAnalysis.UseVisualStyleBackColor = true;
@@ -64,62 +65,69 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(14, 237);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 16);
+            this.label1.Size = new System.Drawing.Size(179, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "Количество слов =";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(398, 284);
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(559, 282);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(175, 171);
+            this.textBox1.Size = new System.Drawing.Size(267, 244);
             this.textBox1.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.Location = new System.Drawing.Point(14, 263);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 16);
+            this.label2.Size = new System.Drawing.Size(119, 20);
             this.label2.TabIndex = 5;
             this.label2.Text = "Топ 10 слов";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(111, 263);
+            this.label3.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(258, 263);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(116, 16);
+            this.label3.Size = new System.Drawing.Size(169, 20);
             this.label3.TabIndex = 6;
             this.label3.Text = "Анти топ 10 слов";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(238, 263);
+            this.label4.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(399, 263);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(131, 16);
+            this.label4.Size = new System.Drawing.Size(189, 20);
             this.label4.TabIndex = 7;
             this.label4.Text = "Топ 10 частых слов";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(395, 263);
+            this.label5.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(556, 263);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(212, 16);
+            this.label5.Size = new System.Drawing.Size(279, 20);
             this.label5.TabIndex = 8;
             this.label5.Text = "Процентное соотношение слов";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(152, 197);
+            this.button1.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(414, 197);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(131, 37);
+            this.button1.Size = new System.Drawing.Size(412, 37);
             this.button1.TabIndex = 9;
             this.button1.Text = "Открыть файл";
             this.button1.UseVisualStyleBackColor = true;
@@ -129,21 +137,9 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(289, 197);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(131, 37);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Сохранить результат";
-            this.button2.UseCompatibleTextRendering = true;
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(702, 533);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(839, 533);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -153,8 +149,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonAnalysis);
             this.Controls.Add(this.textBox);
+            this.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Name = "Form1";
-            this.Text = "АнАлИзАтОр_24н9";
+            this.Text = "Анализатор Текста";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,7 +168,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
