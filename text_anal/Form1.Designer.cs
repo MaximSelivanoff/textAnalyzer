9b9f32f54987f86c75cbdd75d84ec2ebd3b33c9b
PC <PC@LAPTOP-LSIS1K39>
2023-02-01 10:12:39 +0300

    Добавьте файлы проекта.

---
+++ text_anal/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace text_anal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.buttonAnalysis = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(13, 20);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(677, 171);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = "Писатб сюда свои поэмы";
+            // 
+            // buttonAnalysis
+            // 
+            this.buttonAnalysis.Location = new System.Drawing.Point(15, 197);
+            this.buttonAnalysis.Name = "buttonAnalysis";
+            this.buttonAnalysis.Size = new System.Drawing.Size(131, 37);
+            this.buttonAnalysis.TabIndex = 1;
+            this.buttonAnalysis.Text = "Анализироватб";
+            this.buttonAnalysis.UseVisualStyleBackColor = true;
+            this.buttonAnalysis.Click += new System.EventHandler(this.buttonAnalysis_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество слов =";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(398, 284);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(175, 171);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Топ 10 слов";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Анти топ 10 слов";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(238, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Топ 10 частых слов";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(395, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(212, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Процентное соотношение слов";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(152, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 37);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Открыть файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(289, 197);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 37);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Сохранить результат";
+            this.button2.UseCompatibleTextRendering = true;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(702, 533);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonAnalysis);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "АнАлИзАтОр_24н9";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button buttonAnalysis;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ text_anal/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+namespace text_anal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+        }
+        class StringMax : IComparer<string>
+        {
+            public int Compare(string o1, string o2)
+            {
+                if (o1.Length > o2.Length)
+                    return -1;
+                else if (o1.Length < o2.Length)
+                    return 1;
+                return 0;
+            }
+        }
+        class StringMin : IComparer<string>
+        {
+            public int Compare(string o1, string o2)
+            {
+                if (o1.Length > o2.Length)
+                    return 1;
+                else if (o1.Length < o2.Length)
+                    return -1;
+                return 0;
+            }
+        }
+        private void buttonAnalysis_Click(object sender, EventArgs e)
+        {
+            string text = textBox.Text;
+            // при помощи регулярного выражения находим все слова
+            Regex rg = new Regex("[а-яa-z]+", RegexOptions.IgnoreCase);
+            MatchCollection matches = rg.Matches(text);
+            List<string> str = new List<string>();
+            Dictionary<string, int> stat = new Dictionary<string, int>();
+            foreach (Match m in matches)
+            {
+                str.Add(m.ToString().ToLower());
+                if (stat.ContainsKey(m.ToString().ToLower()))
+                    stat[m.ToString().ToLower()]++;
+                else
+                    stat.Add(m.ToString().ToLower(), 1);
+            }
+            label1.Text = "Количество слов = " + matches.Count.ToString();
+            StringMax sort1 = new StringMax();
+            str.Sort(sort1);
+            label2.Text = "Топ 10 слов:";
+            int i = 0;
+            foreach (string s in str)
+            {
+                label2.Text += ("\n" + (i+1) + ") " + s);
+                i++;
+                if (i > 9)
+                    break;
+            }
+            StringMin sort2 = new StringMin();
+            str.Sort(sort2);
+            label3.Text = "Анти топ 10 слов:";
+            i = 0;
+            foreach (string s in str)
+            {
+                label3.Text += ("\n" + (i + 1) + ") " + s);
+                i++;
+                if (i > 9)
+                    break;
+            }
+            textBox1.Text = "";
+            foreach (var word in stat)
+                textBox1.Text += word.Key + " - " + ((float)word.Value / (float)matches.Count * 100).ToString() + "%\r\n";          
+            List<string> topFreqWords = new List<string>();
+            string max_word = null;
+            i = 10;
+            if (stat.Count < 10)
+                i = stat.Count;
+            for (; i > 0; i--)
+            {
+                int max = 0;
+                foreach (var word in stat)
+                {
+                    if (word.Value > max)
+                    {
+                        max = word.Value;
+                        max_word = word.Key;
+                    }
+                }
+                topFreqWords.Add(max_word);
+                stat.Remove(max_word);
+            }
+            label4.Text = "Топ 10 частых слов";
+            foreach (string word in topFreqWords)
+                label4.Text += "\n" + word;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            // читаем файл в строку
+            string fileText = System.IO.File.ReadAllText(filename);
+            textBox.Text = fileText;
+            MessageBox.Show("Файл открыт");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = saveFileDialog1.FileName;
+            // сохраняем текст в файл
+            string saveText = label2.Text + "\n" + label3.Text + "\n" + label4.Text + "\n" + label5.Text + "\n" + textBox1.Text;
+            System.IO.File.WriteAllText(filename, saveText);
+            MessageBox.Show("Файл сохранен");
+        }
+    }
+}
